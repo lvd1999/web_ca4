4da54dba4fbd32f686f37a759d30e899be56607f
callMeDrStrange <47150136+callMeDrStrange@users.noreply.github.com>
2019-03-19 16:07:19 +0000

    version 1.0

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if (!isset($login_email)) {
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>xCoach</title>
+        <title>AMMU-Nation</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="" name="keywords">
         <meta content="" name="description">
