437c0f1924faf93dc4af43839f24fd8d4cc9c7d4
callMeDrStrange <47150136+callMeDrStrange@users.noreply.github.com>
2019-03-21 14:34:48 +0000

    added a home page after login 1.3 added a new button(manager) in nav bar

--- view/header.php
+++ view/header.php
@@ -30,6 +30,9 @@ $last_name = $_SESSION['last_name'];
                                 <li>
                                     <a class="page-scroll" href="#contact">Contact</a>
                                 </li>
+                                <li>
+                                    <a class="page-scroll" href="#contact">Manager</a>
+                                </li>
                                 <li class="dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                         <?php
